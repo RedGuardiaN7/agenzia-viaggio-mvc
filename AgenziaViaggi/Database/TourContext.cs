4be79458a62bd336e585ef82cdede53439cc83d4
RedGuardiaN7 <artiom.str@hotmail.it>
2023-01-16 15:06:19 +0100

    Aggiunto context

---
+++ AgenziaViaggi/Database/TourContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using AgenziaViaggi.Models;
+
+namespace AgenziaViaggi.Database
+{
+    public class TourContext : DbContext
+    {
+        public DbSet<Tour> Tours { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=TourDB;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+
+    }
+}
