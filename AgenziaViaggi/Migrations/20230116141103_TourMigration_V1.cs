e2498ca3225bd929dca378e6c63cbc7a3e497d5f
RedGuardiaN7 <artiom.str@hotmail.it>
2023-01-16 15:13:21 +0100

    Prima migration per il DB

---
+++ AgenziaViaggi/Migrations/20230116141103_TourMigration_V1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgenziaViaggi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TourMigrationV1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tours",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "varchar(150)", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "varchar(350)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Destinations = table.Column<string>(type: "text", nullable: false),
+                    Days = table.Column<short>(type: "smallint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tours", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tours");
+        }
+    }
+}
