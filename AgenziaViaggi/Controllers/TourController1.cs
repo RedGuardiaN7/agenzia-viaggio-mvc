aa6aff3e87fb02db3bfee3ef2867cd9bfeccc5f5
GioPa97 <giovannipagliara97@.com>
2023-01-16 14:51:54 +0100

    Aggiunto TourController

---
+++ AgenziaViaggi/Controllers/TourController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgenziaViaggi.Controllers
+{
+    public class TourController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
